ed078d37ca3973fc3ecfb10393cb606157281b90
Amandeep Singh <amandeep11121993@gmail.com>
2020-07-22 17:54:32 +0530

    ec2 service

--- services/ec2/main.tf
+++ services/ec2/main.tf
@@ -1,10 +1,10 @@
 module "vpc" {
-  source       = "./resources/vpc"
+  source       = "../../resources/vpc"
   primary_cidr = "172.20.0.0/16"
 }
 
 module "ec2" {
-  source            = "./resources/ec2"
+  source            = "../../resources/ec2"
   public_subnet_id  = module.vpc.public_subnet_ids.0
   vpc_id            = module.vpc.vpc_id
   instance_name = var.instance_name
