8e5f0a4a48781028790a14aa59f1e9ea3b0abd34
Amandeep Singh <amandeep11121993@gmail.com>
2020-08-05 16:48:00 +0530

    aks module with default settings

---
+++ resources/aks/variable.tf
@@ -0,0 +1,30 @@
+variable "resourcename" {
+  default = "aman-aks"
+}
+variable "clustername" {
+  default = "aman"
+}
+variable "location" {
+  default = "East US"
+}
+variable "dnspreffix" {
+  default = "aman-aks"
+}
+variable "size" {
+  default = "Standard_D2_v2"
+}
+variable "agentnode" {
+  default = "1"
+}
+variable "subscription_id" {
+  default = "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"
+}
+variable "client_id" {
+  default = "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"
+}
+variable "client_secret" {
+  default = "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"
+}
+variable "tenant_id" {
+  default = "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"
+}
